2b9db6645132680fb900096e6b4295db470540a6
ivan.k <ivan.k@uniweb.ua>
2025-05-12 23:07:48 +0300

    Добавьте файлы проекта.

---
+++ wpf_pract_3(3)/LoginControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_pract_3_3_
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginControl.xaml
+    /// </summary>
+    public partial class LoginControl : UserControl
+    {
+        public LoginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameBox.Text;
+            var password = PasswordBox.Password;
+
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                MessageBlock.Text = $"Вітаємо, {username}!";
+            }
+            else
+            {
+                MessageBlock.Text = "Будь ласка, введіть обидва поля.";
+                MessageBlock.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            }
+        }
+    }
+}
